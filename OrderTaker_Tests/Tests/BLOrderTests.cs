d41bc6487298d79786c877d90d47bbc76bcbf92b
marcin99xxx <marcin.mrozek1305@gmail.com>
2022-08-11 03:15:22 +0200

    Test - 0

---
+++ OrderTaker_Tests/Tests/BLOrderTests.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer;
+using Models;
+using Moq;
+using Repositories.UOW;
+using Xunit;
+
+namespace OrderTakerTests.Tests
+{
+    public class BLOrderTests
+    {
+        private readonly OrdersDbContext _context;
+
+        [Fact]
+        public void BLOrder_GetOrderById_ValidId()
+        {
+            //Arrange
+            var mockOrderRepo = new MockOrderRepository().MockGetOrderByIdInvalid();
+            var Uow = new UnitOfWork(_context);
+            var BLOrder = new BLOrder(Uow);
+
+            //Act
+            var myOrder = BLOrder.GetOrderById(1);
+
+            //Assert
+            mockOrderRepo.VerifyGetOrderById(Times.Never());
+        }
+    }
+}
