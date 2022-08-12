d34bd088aa1d16cd9c3dbd06880d7a2d815e7a4c
marcin99xxx <marcin.mrozek1305@gmail.com>
2022-08-12 03:30:33 +0200

    Finish

---
+++ OrderTaker_Tests/Fakes/FakeFoodItemRepository.cs
@@ -0,0 +1,33 @@
+﻿using Models;
+using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderTaker_Tests.Fakes
+{
+    public class FakeFoodItemRepository : IFoodItemRepository
+    {
+        private List<FoodItem> _foods = new List<FoodItem>()
+      {
+         new FoodItem()
+        {
+            FoodItemId = 1,
+            FoodItemName = "test",
+            Price = 5.00m,
+        }
+      };
+
+        public IEnumerable<FoodItem> GetAllFoodItems()
+        {
+            return _foods;
+        }
+
+        public decimal GetFoodItemPrice(int id)
+        {
+            return _foods.Single(x => x.FoodItemId == id).Price;
+        }
+    }
+}
---
+++ OrderTaker_Tests/Stubs/StubFoodItemRepository.cs
@@ -0,0 +1,31 @@
+﻿using Models;
+using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderTaker_Tests.Stubs
+{
+    public class StubFoodItemRepository : IFoodItemRepository
+    {
+        public IEnumerable<FoodItem> GetAllFoodItems()
+        {
+            return new List<FoodItem>()
+            {
+              new FoodItem()
+              {
+               FoodItemId = 1,
+               FoodItemName = "test",
+               Price = 5.00m,
+              }
+            };
+        }
+
+        public decimal GetFoodItemPrice(int id)
+        {
+            return 5.00m;
+        }
+    }
+}
